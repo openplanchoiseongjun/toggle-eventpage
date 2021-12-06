a990a8eedefcaca9edb7f3e92a630235933a56ee
openplanchoiseongjun <csj2702@openyourplan.com>
2021-12-06 17:42:02 +0900

    json

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,27 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+interface DataList {
+  screen_moving_div_code: string;
+  screen_moving_code: string;
+  screen_moving_url: string;
+}
 const Home: NextPage = () => {
-  function sendMessage(msg: string) {
+  function sendMessage(msg: DataList) {
     (window as any).android.setMessage(msg);
   }
   const callNative = () => {
+    const data: DataList = {
+      screen_moving_div_code: "CM03000003",
+      screen_moving_code: "",
+      screen_moving_url:
+        "https://kr.trip.com/sale/w/2348/weekendsupersale.html?locale=ko_kr&AllianceID=1299066&SID=3763636",
+    };
     // window.webkit.messageHandler.callbackHandler.postMessage("메세지호출");
     if ((window as any).webkit) {
-      (window as any).webkit.messageHandlers.iosMessage.postMessage(
-        "메세지호출1"
-      );
+      (window as any).webkit.messageHandlers.iosMessage.postMessage(data);
     }
-    if ((window as any).android) sendMessage("안드로이드메세지호출");
+    if ((window as any).android) sendMessage(data);
   };
 
   return (
